1f1d44e6988ec4c59a4e8a269caee7e0eedacb72
Animesh <animeshpandey070@gmail.com>
2020-04-27 11:20:33 +0530

    Exposed API for Movie Info and registered Eureka Discovery Client

---
+++ src/main/java/com/microservice/movieinfoservice/controller/MovieInfoController.java
@@ -0,0 +1,19 @@
+package com.microservice.movieinfoservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.movieinfoservice.model.MovieInfo;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInfoController {
+
+	@GetMapping("/{movieId}")
+	public MovieInfo getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new MovieInfo(movieId, "Movie Name");
+	}
+	
+}
